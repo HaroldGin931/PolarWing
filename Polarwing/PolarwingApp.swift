2d71a771c6ba4d4f6c62a576b1f3f1b49d0aa59e
Harold <20009626+HaroldGin931@users.noreply.github.com>
2025-11-22 19:03:56 +0800

    Initial Commit

---
+++ Polarwing/PolarwingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolarwingApp.swift
+//  Polarwing
+//
+//  Created by Harold on 2025/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PolarwingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PolarwingTests/PolarwingTests.swift
@@ -0,0 +1,17 @@
+//
+//  PolarwingTests.swift
+//  PolarwingTests
+//
+//  Created by Harold on 2025/11/22.
+//
+
+import Testing
+@testable import Polarwing
+
+struct PolarwingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
